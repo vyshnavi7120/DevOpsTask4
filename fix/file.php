fe1fcc45c65bdcd376a55c3e444cfbd774c782c7
vyshnavi7120 <62387738+vyshnavi7120@users.noreply.github.com>
2020-08-02 15:26:13 +0530

    created

--- fix/file.php
+++ fix/file.php
@@ -1,11 +1,10 @@
 <pre>
 <?php
 print "Hi";
-print "Hello";
-print "Welcome to Linux world";
-print "My page";
-print "Talla Vyshnavi";
-print "Talla Nithish Kumar";
-print 'ifconfig';
+print " Hello";
+print " Welcome to Linux world";
+print " My page";
+print " Talla Vyshnavi";
+print " Talla Nithish Kumar";
 ?>
 </pre>
